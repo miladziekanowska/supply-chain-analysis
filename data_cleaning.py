5a47e64a2525673d2f86d5d45204f69a03f60908
Mila <miloslawadziekanowska@gmail.com>
2023-08-24 15:31:14 +0200

    Added data_cleaning and supply_chain_data_cleaned.csv, also altered Initial_analysis.ipynb

--- data_cleaning.py
+++ data_cleaning.py
@@ -20,14 +20,23 @@ product_name = create_dictionary(df, 'Product Card Id', 'Product Name')
 department_name = create_dictionary(df, 'Department Id', 'Department Name')
 category_name = create_dictionary(df, 'Category Id', 'Category Name')
 
+# create locations dataset
+store_locations_df = pd.DataFrame({'Latitude': df['Latitude'],
+                                   'Longitude': df['Longitude']})
+store_locations_df = store_locations_df.drop_duplicates()
+
+
 # delete unnessesary columns
 df = df.drop(['Customer Email', 'Customer Fname', 'Customer Lname',
               'Customer Password', 'Customer Street', 'Customer Zipcode',
               'Order Zipcode', 'Product Description', 'Product Image',
               'Product Category Id', 'Category Name', 'Department Name',
-              'Order Customer Id','Order Item Cardprod Id', 'Product Name'], axis=1)
+              'Order Customer Id','Order Item Cardprod Id', 'Product Name',
+             'Latitude', 'Longitude', 'Delivery Status', 'Late_delivery_risk',
+             'Order City', 'Sales', 'Product Status'], axis=1)
 
-#dealing with the shipping and order date columns
+""" 
+# dealing with the shipping and order date columns (normally should be here, will use while analysing to save memory)
 df = df.rename(columns={'shipping date (DateOrders)': 'Shipping date',
                        'order date (DateOrders)': 'Order date'})
 
@@ -45,3 +54,8 @@ df = df.drop(['Shipping date', 'Order date'], axis=1)
 # feature engineering -> create a column representing the difference between expected and realistic shipping time
 
 df['Target shipping days'] = df['Days for shipping (real)'] - df['Days for shipment (scheduled)']
+
+"""
+
+
+df.to_csv('data/supply_chain_cleaned.csv')
